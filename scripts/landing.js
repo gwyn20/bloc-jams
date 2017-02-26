764f62cda81c45430198a8656eeb6af19d23a053
Chris Jones <gwyn20@gmail.com>
2017-02-25 19:56:38 -0500

    Update landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,7 +4,7 @@ var animatePoints = function(points) {
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
         points[index].style.transform = 'scaleX(1) translateY(0)';
-        points[index].style.msTransform = 'scaleX(1) translateY(0)';
+        points[index].style.msTransform = 'git scaleX(1) translateY(0)';
         points[index].style.WebkitTransform = 'scaleX(1) translateY(0)';
     };
     
